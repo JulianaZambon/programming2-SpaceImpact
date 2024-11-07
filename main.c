f6ecd79f08af0494ed36ec1b431a197208321e79
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-07 15:07:19 -0300

    testando

--- main.c
+++ main.c
@@ -1,50 +1,44 @@
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_audio.h>
-#include <stdio.h>
+// Compilação: gcc main.c outro.c -o SpaceImpact $(pkg-config allegro-5 allegro_main-5 allegro_font-5 --libs --cflags)
 
-int main(void)
+#include <allegro5/allegro5.h>     //Biblioteca base do Allegro
+#include <allegro5/allegro_font.h> //Biblioteca de fontes do Allegro
+
+#define X_SCREEN 1000																																										//Definição do tamanho da tela em pixels no eixo x (!)
+#define Y_SCREEN 700
+
+int main()
 {
-    al_init();             // Inicializa a biblioteca Allegro
-    al_install_keyboard(); // Inicializa o teclado
+    al_init();             // Faz a preparação de requisitos da biblioteca Allegro
+    al_install_keyboard(); // Habilita a entrada via teclado (eventos de teclado), no programa
 
-    // Inicializa addons (componentes adicionais)
-    al_init_primitives_addon(); // Para desenhar formas geométricas
-    al_init_image_addon();      // Para carregar imagens
-    al_init_font_addon();       // Para usar fontes básicas
-    al_init_ttf_addon();        // Para fontes TTF
+    ALLEGRO_TIMER *timer = al_create_timer(1.0 / 30.0);   // Cria o relógio do jogo; isso indica quantas atualizações serão realizadas por segundo (30, neste caso)
+    ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue(); // Cria a fila de eventos; todos os eventos (programação orientada a eventos)
+    ALLEGRO_FONT *font = al_create_builtin_font();        // Carrega uma fonte padrão para escrever na tela (é bitmap, mas também suporta adicionar fontes ttf)
+    ALLEGRO_DISPLAY *disp = al_create_display(X_SCREEN, Y_SCREEN);  // Cria uma janela para o programa, define a largura (x) e a altura (y) da tela em píxeis (320x320, neste caso)
 
-    ALLEGRO_TIMER *timer = al_create_timer(1.0 / 60);     // Cria um timer de 60 FPS
-    ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue(); // Cria uma fila de eventos
-    ALLEGRO_FONT *font = al_create_builtin_font();        // Cria uma fonte padrão
-    ALLEGRO_DISPLAY *disp = al_create_display(800, 600);  // Cria uma janela de 800x600 pixels
+    al_register_event_source(queue, al_get_keyboard_event_source());    // Indica que eventos de teclado serão inseridos na nossa fila de eventos
+    al_register_event_source(queue, al_get_display_event_source(disp)); // Indica que eventos de tela serão inseridos na nossa fila de eventos
+    al_register_event_source(queue, al_get_timer_event_source(timer));  // Indica que eventos de relógio serão inseridos na nossa fila de eventos
 
-    al_register_event_source(queue, al_get_keyboard_event_source());
-    al_register_event_source(queue, al_get_display_event_source(disp));
-    al_register_event_source(queue, al_get_timer_event_source(timer));
-
-    ALLEGRO_EVENT event;
-    al_start_timer(timer);
+    ALLEGRO_EVENT event;   // Variável que guarda um evento capturado, sua estrutura é definida em: https://www.allegro.cc/manual/5/ALLEGRO_EVENT
+    al_start_timer(timer); // Função que inicializa o relógio do programa
     while (1)
-    {
-        al_wait_for_event(queue, &event);
+    {                                     // Laço principal do programa
+        al_wait_for_event(queue, &event); // Função que captura eventos da fila, inserindo os mesmos na variável de eventos
 
         if (event.type == 30)
-        {
-            al_clear_to_color(al_map_rgb(0, 0, 0));
-            al_flip_display();
+        {                                           // O evento tipo 30 indica um evento de relógio, ou seja, verificação se a tela deve ser atualizada (conceito de FPS)
+            al_clear_to_color(al_map_rgb(0, 0, 0)); // Substitui tudo que estava desenhado na tela por um fundo preto
+            al_flip_display();                      // Insere as modificações realizadas nos buffers de tela
         }
         else if (event.type == 42)
-            break;
+            break; // Evento de clique no "X" de fechamento da tela. Encerra o programa graciosamente.
     }
 
-    al_destroy_display(font);
-    al_destroy_display(disp); // Destroi a janela
-    al_destroy_timer(timer);
-    al_destroy_event_queue(queue);
+    al_destroy_font(font);         // Destrutor da fonte padrão
+    al_destroy_display(disp);      // Destrutor da tela
+    al_destroy_timer(timer);       // Destrutor do relógio
+    al_destroy_event_queue(queue); // Destrutor da fila
 
     return 0;
 }
