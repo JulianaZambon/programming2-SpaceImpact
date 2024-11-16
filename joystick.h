72ba37ccd936f1b0be8362b48a44c4b8f02d9d7c
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-15 21:58:21 -0300

    Update joystick.h

--- joystick.h
+++ joystick.h
@@ -1,28 +1,19 @@
-#include <stdlib.h>
-#include "Joystick.h"
+#ifndef __JOYSTICK__ // Guardas de inclusão 
+#define __JOYSTICK__ // Guardas de inclusão 
 
-joystick *joystick_create()
-{ // Implementação da função "joystick_create"
+typedef struct
+{                        // Definição da estrutura de um controle 
+    unsigned char right; // Botão de movimentação à direta 
+    unsigned char left;  // Botão de movimentação à esquerda 
+    unsigned char up;    // Botão de movimentação para cima 
+    unsigned char down;  // Botão de movimentação para baixo 
+} joystick;              // Definição do nome da estrutura 
 
-    joystick *element = (joystick *)malloc(sizeof(joystick)); // Aloca memória na heap para um novo controle
-    if (!element)
-        return NULL;    // Se a alocação não deu certo, retorna erro
-    element->right = 0; // Insere o estado de desligado para o botão de movimentação à esquerda
-    element->left = 0;  // Insere o estado de desligado para o botão de movimentação à direita
-    element->up = 0;    // Insere o estado de desligado para o botão de movimentação para cima
-    element->down = 0;  // Insere o estado de desligado para o botão de movimentação para baixo
-    element->fire = 0;  // Insere o estado de desligado para o botão de disparo (!)
-    return element;     // Retorna o novo controle
-}
+joystick *joystick_create();              // Protótipo da função de criação de um controle 
+void joystick_destroy(joystick *element); // Protótipo da função de destruição de um controle 
+void joystick_right(joystick *element);   // Protótipo da função de ativação/desativação do botão de movimentação à direita 
+void joystick_left(joystick *element);    // Protótipo da função de ativação/desativação do botão de movimentação à esquerda 
+void joystick_up(joystick *element);      // Protótipo da função de ativação/desativação do botão de movimentação para cima 
+void joystick_down(joystick *element);    // Protótipo da função de ativação/desativação do botão de movimentação para baixo 
 
-void joystick_destroy(joystick *element) { free(element); } // Implementação da função "joystick_destroy"; libera a memória do elemento na heap
-
-void joystick_left(joystick *element) { element->left = element->left ^ 1; } // Implementação da função "joystick_left"; muda o estado do botão
-
-void joystick_right(joystick *element) { element->right = element->right ^ 1; } // Implementação da função "joystick_right"; muda o estado do botão
-
-void joystick_up(joystick *element) { element->up = element->up ^ 1; } // Implementação da função "joystick_up"; muda o estado do botão
-
-void joystick_down(joystick *element) { element->down = element->down ^ 1; } // Implementação da função "joystick_down"; muda o estado do botão
-
-void joystick_fire(joystick *element) { element->fire = element->fire ^ 1; } // Implementação da função "joystick_fire"; muda o estado do botão (!)
+#endif // Guardas de inclusão 
