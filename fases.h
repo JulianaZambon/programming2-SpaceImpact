0ba827a560415b397d283590d013f1e8d8910e90
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-17 17:20:45 -0300

    Update fases.h

--- fases.h
+++ fases.h
@@ -17,6 +17,22 @@ typedef struct
 fase *criar_fase1();
 // função de criação da fase 2
 fase *criar_fase2();
+
+// Função de criação do loop do background
+void draw_moving_background(ALLEGRO_BITMAP *background, float *offset, float speed, int screen_width);
+
+// Função para verificar a colisão entre o jogador e os inimigos
+unsigned char collision_2D(player *player_1, enemy *enemy_1, enemy *enemy_2, boss *boss_1);
+
+// função que verifica se um projétil acertou um inimigo ou chefe
+unsigned char check_kill(player *killer, enemy *victim);
+
+// função que atualiza o posicionamento de projéteis conforme o movimento dos mesmos
+void update_projeteis(player *player_1);
+
+// Função para atualizar a posição do jogador conforme os comandos do controle e conforme colisões
+void update_position(player *player_1, enemy *enemy_1, enemy *enemy_2, boss *boss_1);
+
 // função de destruição de uma fase
 void fase_destroy(fase *element);
 
