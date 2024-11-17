1574bd8590c9e07c6fec7b4c210d88a16c9811c0
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-16 21:28:08 -0300

    Update inimigos.h

--- inimigos.h
+++ inimigos.h
@@ -2,24 +2,30 @@
 #define __INIMIGOS__ // Guardas de inclusão
 
 #include <allegro5/allegro.h> // Inclusão necessária para usar ALLEGRO_BITMAP
+#include "arma.h"             // Inclusão necessária para usar a estrutura arma
 
 typedef struct
 {
-    float x;                // Posição X do inimigo
-    float y;                // Posição Y do inimigo
-    float speed;            // Velocidade do movimento
-    int hp;                 // Pontos de vida
-    int type;               // Tipo de inimigo (diferencia os 4 inimigos)
-    ALLEGRO_BITMAP *sprite; // Sprite do inimigo
+    unsigned char side;           // Tamanho da lateral de um inimigo
+    unsigned char face;           // A face principal do inimigo, algo como a sua "frente"
+    unsigned char hp;             // Quantidade de vida do inimigo, em unidades
+    unsigned short x;             // Posição X do centro do inimigo
+    unsigned short y;             // Posição Y do centro do inimigo                        
+    int type;                     // Tipo de inimigo (diferencia os 4 inimigos)
+    ALLEGRO_BITMAP *enemy_sprite; // Sprite do inimigo
+    int can_shoot;                // Para determinar se o inimigo pode atirar
+    arma *arma;                   // Elemento para realizar disparos no jogo
 } enemy;
 
-// Função para criar o inimigo
-enemy *create_enemy(float x, float y, float speed, int health, int type, const char *sprite_path);
-// Função para desenhar o inimigo
-void draw_enemy(const enemy *enemies);
-// Função para mover o inimigo
-void move_enemy(enemy *enemies, unsigned char steps, unsigned char trajectory, unsigned short max_x, unsigned short max_y);
-// Função para destruir o inimigo
-void destroy_enemy(enemy *enemies);
+// função de criação de um inimigo
+enemy *enemy_create(unsigned char side, unsigned char face, short x, unsigned short y, unsigned short max_x, unsigned short max_y);
+// função de movimentação do inimigo
+void enemy_move(enemy *element, unsigned char steps, unsigned char trajectory, unsigned short max_x, unsigned short max_y);
+// função de desenho do inimigo
+void enemy_draw(enemy *element);	  
+// funcao de disparo do inimigo
+void enemy_shot(enemy *element);
+// função de destruição do inimigo
+void enemy_destroy(enemy *element); 
 
 #endif // Guardas de inclusão
