49222ff7cdac6421fb77d0d76aac71726bf755f3
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-16 22:07:04 -0300

    Update fases.h

--- fases.h
+++ fases.h
@@ -1,10 +1,14 @@
 #ifndef __FASES__ // Guardas de inclusão
 #define __FASES__ // Guardas de inclusão
 
-typedef struct 
+#include "inimigos.h"
+#include "chefes.h"
+#include "jogador.h"
+
+typedef struct
 {
-    /* data */
+    enemy enemies[NUM_INIMIGOS]; // Vetor de inimigos
+    boss bosses[NUM_BOSSES];     // Vetor de chefes
 } fases;
 
-
-#endif 
+#endif
