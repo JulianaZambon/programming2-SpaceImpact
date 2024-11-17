30bb36a31c61af05bf0f998f0d70b8cf59356158
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-17 16:58:52 -0300

    Update fases.h

--- fases.h
+++ fases.h
@@ -1,6 +1,7 @@
 #ifndef __FASES__ // Guardas de inclusão
 #define __FASES__ // Guardas de inclusão
 
+#include <allegro5/allegro.h> // Inclusão necessária para usar ALLEGRO_BITMAP
 #include "inimigos.h"
 #include "chefes.h"
 #include "jogador.h"
@@ -9,6 +10,16 @@ typedef struct
 {
     enemy enemies[NUM_INIMIGOS]; // Vetor de inimigos
     boss bosses[NUM_BOSSES];     // Vetor de chefes
-} fases;
+    ALLEGRO_BITMAP *background;  // Imagem de fundo
+} fase;
+
+// função de criação do loop do background
+void draw_moving_background(ALLEGRO_BITMAP *background, float *offset, float speed, int screen_width);
+// função de criação da fase 1
+fase *criar_fase1();
+// função de criação da fase 2
+fase *criar_fase2();
+// função de destruição de uma fase
+void fase_destroy(fase *element);
 
 #endif
