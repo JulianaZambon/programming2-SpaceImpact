69b1609d10eea5e68898f23eb40b826384d33b4f
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-18 16:28:06 -0300

    Update configuracoes.h

--- configuracoes.h
+++ configuracoes.h
@@ -3,9 +3,8 @@
 
 #define X_SCREEN 1140
 #define Y_SCREEN 640
+#define BACKGROUND_SPEED 3
 #define ANIMATION_DELAY_PLAYER 3 // Ajuste a velocidade da animação (quanto maior, mais lento)
-
-// função de criação do loop do background
-void draw_moving_background(ALLEGRO_BITMAP *background, float *offset, float speed, int screen_width);
+#define ANIMATION_DELAY_ENEMY 3  // Ajuste a velocidade da animação (quanto maior, mais lento)
 
 #endif
