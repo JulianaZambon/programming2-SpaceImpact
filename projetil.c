9c2588441c6c9357b6cd8214310b01965f886649
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-24 21:39:39 -0300

    Update projetil.c

--- projetil.c
+++ projetil.c
@@ -2,25 +2,22 @@
 #include "projetil.h"
 
 projetil *projetil_create(unsigned short x, unsigned short y, unsigned char trajectory, projetil *next)
-{ 
-    if ((trajectory < 0) || (trajectory > 1))
-        return NULL; // Verifica se a trajetória informada para o projétil é válida
+{
+    if (trajectory < 0 || trajectory > 1) // Verifica se a trajetória informada para o projétil é válida
+        return NULL;
 
     projetil *new_projetil = (projetil *)malloc(sizeof(projetil)); // Aloca memória na heap para uma instância de projétil
     if (!new_projetil)
-        return NULL;
-
-    new_projetil->x = x;                      // Armazena a posição X informada
-    new_projetil->y = y;                      // Armazena a posição Y informada
-    new_projetil->trajectory = trajectory;    // Armazena a trajetória informada
+        return NULL;                              // Verifica o sucesso da alocação de memória; retorna NULL em caso de falha
+    new_projetil->x = x;                          // Armazena a posição X informada
+    new_projetil->y = y;                          // Armazena a posição Y informada
+    new_projetil->trajectory = trajectory;        // Armazena a trajetória informada
     new_projetil->next = (struct projetil *)next; // Armazena o próximo projétil informado
-
-    return new_projetil;                      
+    return new_projetil;                          // Retorna a instância criada de projétil
 }
 
 void projetil_move(projetil *elements)
-{ // Implementação da função "projetil_move"
-
+{
     for (projetil *index = elements; index != NULL; index = (projetil *)index->next)
     { // Para cada projétil presente na lista de projéteis informada
         if (!index->trajectory)
@@ -31,6 +28,6 @@ void projetil_move(projetil *elements)
 }
 
 void projetil_destroy(projetil *element)
-{ 
+{
     free(element); // Libera a memória da instância de projétil
 }
