333367b3d1fe3572274e4aad4d334a9f809a1166
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-28 23:17:50 -0300

    Update configuracoes.h

--- configuracoes.h
+++ configuracoes.h
@@ -5,9 +5,6 @@
 #define Y_SCREEN 640
 #define VELOCIDADE_BACKGROUND 3 // Ajuste a velocidade do background (quanto maior, mais rápido)
 #define ANIMATION_DELAY_JOGADOR 3 // Ajuste a velocidade da animação (quanto maior, mais lento)
-#define ANIMATION_DELAY_INIMIGO 3  // Ajuste a velocidade da animação (quanto maior, mais lento)
-#define ARMA_COOLDOWN 10 // Ajuste o tempo de cooldown da arma (quanto maior, mais lento)
-#define PROJETIL_MOVE 5 // Ajuste a velocidade do projetil (quanto maior, mais rápido)
 
 #include "jogador.h"
 #include "inimigos.h"
