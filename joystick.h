c8a9588c1e29794b8ecfa42377e57df7550e8f41
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-29 20:21:39 -0300

    Update joystick.h

--- joystick.h
+++ joystick.h
@@ -1,30 +1,35 @@
 #ifndef __JOYSTICK__ // Guardas de inclusão
 #define __JOYSTICK__ // Guardas de inclusão
 
+/*-----------------------------------------------------------------------------------------*/
+/* ESTRUTURA */
+
+// Estrutura para o controle do jogador
 typedef struct
 {
-    unsigned char right; // Botão de movimentação à direta
-    unsigned char left;  // Botão de movimentação à esquerda
-    unsigned char up;    // Botão de movimentação para cima
-    unsigned char down;  // Botão de movimentação para baixo
-    unsigned char fire;  // Botão de disparo 
-
+    unsigned char direita;  // Botão de movimentação à direta
+    unsigned char esquerda; // Botão de movimentação à esquerda
+    unsigned char cima;     // Botão de movimentação para cima
+    unsigned char baixo;    // Botão de movimentação para baixo
+    unsigned char fogo;     // Botão de disparo
 } joystick;
 
+/*-----------------------------------------------------------------------------------------*/
+/* PROTÓTIPOS DE FUNÇÕES */
 
-// função de criação de um controle
-joystick *joystick_create();
-// função de destruição de um controle
-void joystick_destroy(joystick *element);
-// função de ativação/desativação do botão de movimentação à direita
-void joystick_right(joystick *element);
-// função de ativação/desativação do botão de movimentação à esquerda
-void joystick_left(joystick *element);
-// função de ativação/desativação do botão de movimentação para cima
-void joystick_up(joystick *element);
-// função de ativação/desativação do botão de movimentação para baixo
-void joystick_down(joystick *element); 
-// função de ativação/desativação do botão de disparo
-void joystick_fire(joystick *element);	 
+// Função de criação de um controle
+joystick *criar_joystick();
+// Função de destruição de um controle
+void destroi_joystick(joystick *element);
+// Função de ativação/desativação do botão de movimentação à direita
+void joystick_direita(joystick *element);
+// Função de ativação/desativação do botão de movimentação à esquerda
+void joystick_esquerda(joystick *element);
+// Função de ativação/desativação do botão de movimentação para cima
+void joystick_cima(joystick *element);
+// Função de ativação/desativação do botão de movimentação para baixo
+void joystick_baixo(joystick *element);
+// Função de ativação/desativação do botão de disparo
+void joystick_fogo(joystick *element);
 
 #endif // Guardas de inclusão
