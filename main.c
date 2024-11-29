51c1998354b16e82609997fe76a43c5fb49df603
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-29 19:55:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,15 +4,19 @@
 #include <allegro5/allegro_primitives.h>
 #include <stdio.h>
 
+// Inclusão de bibliotecas locais
 #include "configuracoes.h"
+#include "background.h"
 #include "jogador.h"
 #include "inimigos.h"
 
-#define PATH_TELA_INICIAL "assets/telas/tela_inicial.png"
-#define PATH_CENARIO_1 "assets/cenarios/cenario1.png"
+/*-----------------------------------------------------------------------------------------*/
+/* DEFINIÇÕES */
+#define TELA_INICIAL "assets/cenarios/tela_inicial.png"
 #define PATH_CENARIO_2 "assets/cenarios/cenario2.png"
 
-#define PATH_JOGADOR "assets/jogador/sprite_jogador.png"
+/*-----------------------------------------------------------------------------------------*/
+/* FUNÇÕES AUXILIARES */
 
 // Atualiza a posição e ações do jogador
 void atualiza_posicao(jogador *jogador_1)
@@ -38,6 +42,8 @@ void atualiza_posicao(jogador *jogador_1)
     mover_projetil(&jogador_1->arma->shots);
 }
 
+/*-----------------------------------------------------------------------------------------*/
+/* MAIN */
 int main()
 {
     // Inicialização do Allegro
@@ -55,35 +61,31 @@ int main()
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(disp));
     al_register_event_source(queue, al_get_timer_event_source(timer));
-
     al_start_timer(timer);
 
+    // Após a tela inicial, o jogo começa
     // Inicialização do jogador
     jogador *jogador_1 = criar_jogador(20, 60, 80, Y_SCREEN / 2, X_SCREEN, Y_SCREEN);
     jogador_1->sprite = al_load_bitmap(PATH_JOGADOR);
     if (!jogador_1)
         return 1;
 
-    // Variáveis de controle do jogador
-    jogador_1->frame_atual = 0;
-
-    // Variáveis de controle de animação
-    unsigned int animation_counter = 0;
-
     // Inicialização do inimigo
     inimigo *inimigo_1 = criar_inimigo(20, 60, X_SCREEN - 50, Y_SCREEN / 2, 2, X_SCREEN, Y_SCREEN);
     if (!inimigo_1)
         return 1;
 
-    // inimigo *lista_inimigos[NUM_INIMIGOS_ORDA] = {NULL};     // Vetor de inimigos
-    // criar_onda_inimigos(lista_inimigos, X_SCREEN, Y_SCREEN, 0); // Cria a onda de inimigos
-
-    // Variáveis de controle de inimigos
     unsigned char trajetoria = 0; // Inicializa o controle de trajetória
 
     // Variáveis de controle de animação
+    unsigned int animation_counter_jogador = 0;
+    unsigned int animation_counter_inimigo = 0;
+    unsigned int animation_counter_fundo = 0;
+
     float background_x = 0;
 
+    /*-----------------------------------------------------------------------------------------*/
+    /* LOOP PRINCIPAL DO JOGO */
     while (1)
     {
         ALLEGRO_EVENT event;
@@ -101,12 +103,9 @@ int main()
             al_draw_bitmap(background, background_x, 0, 0);
             al_draw_bitmap(background, background_x + al_get_bitmap_width(background), 0, 0);
 
-            // Atualiza animação do jogador
-            atualizar_animacao_jogador(jogador_1, &animation_counter, ANIMATION_DELAY_JOGADOR);
-
-            // Atualiza animação do inimigo
-            atualizar_animacao_inimigo(inimigo_1, &animation_counter, ANIMATION_DELAY_INIMIGO);
-            // atualizar_onda_inimigos(lista_inimigos, X_SCREEN, Y_SCREEN, &animation_counter);
+            // Atualiza animação do jogador e do inimigo
+            atualizar_animacao_jogador(jogador_1, &animation_counter_jogador, ANIMATION_DELAY_JOGADOR);
+            atualizar_animacao_inimigo(inimigo_1, &animation_counter_inimigo, ANIMATION_DELAY_INIMIGO);
 
             // Atualiza o jogador e projéteis
             atualiza_posicao(jogador_1);
@@ -117,9 +116,11 @@ int main()
             // Desenha o jogador
             desenhar_jogador(jogador_1);
 
+            // Desenha os corações de HP
+            desenhar_hp(jogador_1, 15, 15);
+
             // Desenha o inimigo
             desenhar_inimigo(inimigo_1);
-            // desenhar_onda_inimigos(lista_inimigos, X_SCREEN, Y_SCREEN);
 
             // Desenha os projéteis do jogador
             for (projetil *p = jogador_1->arma->shots; p != NULL; p = (projetil *)p->proximo)
@@ -127,17 +128,14 @@ int main()
                 desenhar_projetil(p);
             }
 
+            // Atualiza a arma do jogador
             if (jogador_1->arma->timer)
                 atualiza_arma(jogador_1->arma);
 
-            // Desenha os projéteis do inimigo
-            for (projetil *p = inimigo_1->arma->shots; p != NULL; p = (projetil *)p->proximo)
-            {
-                desenhar_projetil(p);
-            }
-
             al_flip_display();
         }
+        /*-----------------------------------------------------------------------------------------*/
+        /* EVENTOS DO JOGADOR */
         else if (event.type == ALLEGRO_EVENT_KEY_DOWN || event.type == ALLEGRO_EVENT_KEY_UP)
         {
             switch (event.keyboard.keycode)
@@ -164,8 +162,8 @@ int main()
             break;
         }
     }
-
-    // Libera recursos
+    /*-----------------------------------------------------------------------------------------*/
+    /* LIBERAÇÃO DE RECURSOS */
     al_destroy_bitmap(jogador_1->sprite);
     al_destroy_bitmap(background);
     al_destroy_display(disp);
