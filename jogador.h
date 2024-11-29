0447c99605da919104f2961e2b076049f01d43de
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-28 23:18:58 -0300

    Update jogador.h

--- jogador.h
+++ jogador.h
@@ -2,34 +2,38 @@
 #define __JOGADOR__ // Guardas de inclusão
 
 #include <allegro5/allegro.h> // Inclusão necessária para usar ALLEGRO_BITMAP
-#include "joystick.h"		  // Inclusão necessária para usar a estrutura joystick
-#include "arma.h"			  // Inclusão necessária para usar a estrutura arma
+
+#include "joystick.h" // Inclusão necessária para usar a estrutura joystick
+#include "arma.h"	  // Inclusão necessária para usar a estrutura arma
 
 // Quantidade de pixels que um jogador se move por passo
-#define PLAYER_STEP 20
+#define JOGADOR_STEP 20
 
 typedef struct
 {
-	unsigned char side;			  // Tamanho da lateral de um jogador
-	unsigned char face;			  // A face principal do jogador, algo como a sua "frente"
-	unsigned char hp;			  // Quantidade de vida do jogador, em unidades 
-	unsigned short x;			  // Posição X do centro do jogador
-	unsigned short y;			  // Posição Y do centro do jogador
-	ALLEGRO_BITMAP *sprite;		  // Adicionado: ponteiro para o sprite do jogador
-	unsigned short current_frame; // Quadro atual (índice no sprite sheet)
-	joystick *control;			  // Elemento de controle da nave no jogo
-	arma *arma;					  // Elemento para realizar disparos no jogo
-} player;
+	unsigned char tam_lateral;	// Tamanho da lateral de um jogador
+	unsigned char face;			// A face principal do jogador, algo como a sua "frente"
+	unsigned char hp;			// Quantidade de vida do jogador, em unidades
+	unsigned short x;			// Posição X do centro do jogador
+	unsigned short y;			// Posição Y do centro do jogador
+	unsigned short frame_atual; // Quadro atual (índice no sprite sheet)
+	joystick *controle;			// elemento de controle da nave no jogo
+	arma *arma;					// elemento para realizar disparos no jogo
+	ALLEGRO_BITMAP *sprite;		// ponteiro para o sprite do jogador
+
+} jogador;
 
 // função de criação de um jogador
-player *player_create(unsigned char side, unsigned char face, short x, unsigned short y, unsigned short max_x, unsigned short max_y);
+jogador *criar_jogador(unsigned char side, unsigned char face, short x, unsigned short y, unsigned short max_x, unsigned short max_y);
 // função de movimentação do jogador
-void player_move(player *element, char steps, unsigned char trajectory, unsigned short max_x, unsigned short max_y); 
+void mover_jogador(jogador *elemento, char steps, unsigned char trajetoria, unsigned short max_x, unsigned short max_y);
 // função de desenho do jogador
-void player_draw(player *element);	  
+void desenhar_jogador(jogador *elemento);
+// função de atualização da animação do jogador
+void atualizar_animacao_jogador(jogador *elemento, unsigned int *animation_counter, unsigned int delay);
 // funcao de disparo do jogador
-void player_shot(player *element);
+void jogador_atira(jogador *elemento);
 // função de destruição do jogador
-void player_destroy(player *element); 
+void destroi_jogador(jogador *elemento);
 
 #endif // Guardas de inclusão
