19364b73423e052f930f0bd9b40e58d88bf334ae
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-29 20:22:21 -0300

    Update inimigos.h

--- inimigos.h
+++ inimigos.h
@@ -2,28 +2,31 @@
 #define __INIMIGOS__ // Guardas de inclusão
 
 #include <allegro5/allegro.h> // Inclusão necessária para usar ALLEGRO_BITMAP
-#include "arma.h"             // Inclusão necessária para usar a estrutura arma
+
+// Inclusões de bibliotecas locais
+#include "arma.h"             
 
 /*-----------------------------------------------------------------------------------------*/
+/* DEFINIÇÕES */
+#define NUM_INIMIGOS 4 // Número de tipos de inimigos
+#define ANIMATION_DELAY_INIMIGO 3 // Velocidade da animação (quanto maior, mais lento)
 
-#define ANIMATION_DELAY_INIMIGO 3  // velocidade da animação (quanto maior, mais lento)
-
+// Definições de inimigos
 #define INIMIGO_STEP 5
 #define INIMIGO1_STEP 3
 #define INIMIGO2_STEP 1
 #define INIMIGO3_STEP 4
 
-#define NUM_INIMIGOS 4 // Número de tipos de inimigos
-
+// Paths dos sprites dos inimigos
 #define PATH_INIMIGO_0 "assets/inimigos/fase1/enemy2.png"
 #define PATH_INIMIGO_1 "assets/inimigos/fase1/enemy4.png"
 #define PATH_INIMIGO_2 "assets/inimigos/fase2/enemy.png"
 #define PATH_INIMIGO_3 "assets/inimigos/fase2/enemy3.png"
 
-#define NUM_INIMIGOS_ORDA 15 // Número de inimigos em uma onda
-
 /*-----------------------------------------------------------------------------------------*/
+/* ESTRUTURAS */
 
+// Estrutura para informações do sprite do inimigo
 typedef struct
 {
     int largura;            // Largura do quadro do sprite
@@ -32,6 +35,7 @@ typedef struct
     ALLEGRO_BITMAP *sprite; // Imagem do sprite
 } inimigo_sprite;
 
+// Estrutura para o inimigo
 typedef struct
 {
     unsigned char tam_lateral;   // Tamanho da lateral de um inimigo
@@ -40,29 +44,59 @@ typedef struct
     unsigned short x;            // Posição X do centro do inimigo
     unsigned short y;            // Posição Y do centro do inimigo
     unsigned short tipo;         // Tipo de inimigo (diferencia os 4 inimigos)
-    unsigned short frame_atual;  // Quadro atual (índice no sprite)
+    unsigned int frame_atual;    // Quadro atual (índice no sprite)
     int pode_atirar;             // Para determinar se o inimigo pode atirar
     arma *arma;                  // Elemento para realizar disparos no jogo
     inimigo_sprite *sprite_info; // Informações sobre o sprite do inimigo
 } inimigo;
 
-// função de criação de um inimigo
+// Estrutura para controlar a onda de inimigos
+// typedef struct
+// {
+//     unsigned short num_inimigos;           // Número de inimigos na onda
+//     unsigned short max_x;                  // Largura máxima da tela
+//     unsigned short max_y;                  // Altura máxima da tela
+//     inimigo *lista_inimigos[MAX_INIMIGOS]; // Lista de inimigos na onda
+//     double proximo_tempo;                  // Próximo tempo de aparição de inimigo
+// } OndaInimigos;
+
+/*-----------------------------------------------------------------------------------------*/
+/* /* PROTÓTIPOS DE FUNÇÕES DO INIMIGO */
+
+// Função de criação de um inimigo
 inimigo *criar_inimigo(unsigned char side, unsigned char face, short x, unsigned short y, unsigned short type, unsigned short max_x, unsigned short max_y);
-// função de movimentação do inimigo
+
+// Função de movimentação do inimigo
 void mover_inimigo(inimigo *elemento, unsigned char steps, unsigned char *trajetoria, unsigned short max_x, unsigned short max_y);
-// função de desenho do inimigo
+
+// Função de desenho do inimigo
 void desenhar_inimigo(inimigo *element);
-// função de atualização da animação do inimigo
+
+// Função de atualização da animação do inimigo
 void atualizar_animacao_inimigo(inimigo *elemento, unsigned int *animation_counter, unsigned int delay);
-// função de criação da onda de inimigos
-void criar_onda_inimigos(inimigo **lista_inimigos, unsigned short max_x, unsigned short max_y, unsigned short tipo);
-// função de desenho da onda de inimigos
-void desenhar_onda_inimigos(inimigo **lista_inimigos, unsigned short max_x, unsigned short max_y);
-// função de movimentação da onda de inimigos
-void atualizar_onda_inimigos(inimigo **lista_inimigos, unsigned short max_x, unsigned short max_y, int *animation_counter);
-// funcao de disparo do inimigo
+
+// Função de disparo do inimigo
 void inimigo_atira(inimigo *element);
-// função de destruição do inimigo
+
+// Função de destruição do inimigo
 void destroi_inimigo(inimigo *element);
 
-#endif // Guardas de inclusão
+/*-----------------------------------------------------------------------------------------*/
+/* FUNÇÕES ONDA DE INIMIGOS */
+
+// // Função de criação de uma nova onda de inimigos
+// OndaInimigos *criar_onda_inimigos(unsigned short max_x, unsigned short max_y);
+
+// // Função de desenho da onda de inimigos
+// void desenhar_onda_inimigos(OndaInimigos *onda);
+
+// // Função de atualização da onda de inimigos
+// void atualizar_onda_inimigos(OndaInimigos *onda);
+
+// // Função de atualização da animação dos inimigos na onda
+// void atualizar_animacao_onda_inimigos(OndaInimigos *onda, unsigned int *animation_counter, unsigned int delay);
+
+// // Função de destruição da onda de inimigos
+// void destruir_onda_inimigos(OndaInimigos *onda);
+
+#endif // __INIMIGOS__
