0fa70a50d21774cbece7005e347e765a894d084f
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-11-30 19:25:41 -0300

    Update projetil.h

--- projetil.h
+++ projetil.h
@@ -1,7 +1,7 @@
 #ifndef __PROJETIL__ // Guardas de inclusão
 #define __PROJETIL__ // Guardas de inclusão
 
-#include <allegro5/allegro.h> // Biblioteca base do Allegro
+#include <allegro5/allegro.h> 
 
 /*-----------------------------------------------------------------------------------------*/
 /* DEFINIÇÕES */
@@ -10,6 +10,12 @@
 /*-----------------------------------------------------------------------------------------*/
 /* ESTRUTURA */
 
+// Estrutura para o ataque especial
+typedef struct ataque_especial
+{
+    int tempo; // Tempo em que o ataque especial dura (max de 5s)
+} ataque_especial;
+
 // Estrutura para o projetil
 typedef struct projetil
 {
@@ -17,8 +23,7 @@ typedef struct projetil
     unsigned short y;          // Local, no eixo y, onde se localiza o projetil
     unsigned char trajetoria;  // Trajetória da projetil
     struct projetil *proximo;  // Próxima projetil, se houver, formando uma lista encadeada
-    struct projetil *especial; // Projetil especial (adquirido no mapa!)
-    // ALLEGRO_BITMAP *imagem;    // Imagem do projétil (feature futura)
+    ataque_especial *especial; // Projetil especial (adquirido no mapa!)
 } projetil;
 
 /*-----------------------------------------------------------------------------------------*/
@@ -26,13 +31,27 @@ typedef struct projetil
 
 // Função de criação de um projetil
 projetil *criar_projetil(unsigned short x, unsigned short y, unsigned char trajectory, projetil *proximo);
-// Função de desenho de um projetil
-void desenhar_projetil(projetil *projetil);
 // Função de movimentação de um projetil
 void mover_projetil(projetil **elements);
 // Função de verificação de colisão de um projetil
 int verificar_colisao_projetil(projetil *projetil, unsigned short x, unsigned short y, unsigned short side);
 // Função de destruição de um projetil
 void destruir_projetil(projetil *element);
+
+/* DESENHO DE PROJETEIS */
+// Função de desenho de um projetil para o jogador
+void desenhar_projetil_jogador(projetil *projetil);
+void desenhar_projetil_especial_jogador_1(projetil *projetil);
+void desenhar_projetil_especial_jogador_2(projetil *projetil);
+// Função de desenho de um projetil para os inimigos
+void desenhar_projetil_inimigo_fase_1(projetil *projetil);
+void desenhar_projetil_inimigo_fase_2(projetil *projetil);
+// Função de desenho de um projetil para os chefes
+void desenhar_projetil_chefe_0(projetil *projetil);
+void desenhar_projetil2_chefe_0(projetil *projetil);
+void desenhar_projetil_chefe_1(projetil *projetil);
+void desenhar_projetil2_chefe_1(projetil *projetil);
+
+/* ATAQUE ESPECIAL */
 
 #endif // Guardas de inclusão
