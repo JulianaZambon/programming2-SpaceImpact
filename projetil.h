b632f7fea1a45f1509b160d27d11ec79950d64bd
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-12-01 19:26:54 -0300

    Update projetil.h

--- projetil.h
+++ projetil.h
@@ -1,14 +1,14 @@
 #ifndef __PROJETIL__ // Guardas de inclusão
 #define __PROJETIL__ // Guardas de inclusão
 
-#include <allegro5/allegro.h> // Biblioteca base do Allegro
+#include <allegro5/allegro.h>
 
 /*-----------------------------------------------------------------------------------------*/
 /* DEFINIÇÕES */
-#define PROJETIL_MOVE 5 // Ajuste a velocidade do projetil (quanto maior, mais rápido)
+#define PROJETIL_MOVE 5 // Velocidade do projetil (quanto maior, mais rápido)
 
 /*-----------------------------------------------------------------------------------------*/
-/* ESTRUTURA */
+/* ESTRUTURAS */
 
 // Estrutura para o ataque especial
 typedef struct ataque_especial
@@ -33,25 +33,33 @@ typedef struct projetil
 projetil *criar_projetil(unsigned short x, unsigned short y, unsigned char trajectory, projetil *proximo);
 // Função de movimentação de um projetil
 void mover_projetil(projetil **elements);
-// Função de verificação de colisão de um projetil
-int verificar_colisao_projetil(projetil *projetil, unsigned short x, unsigned short y, unsigned short side);
 // Função de destruição de um projetil
 void destruir_projetil(projetil *element);
 
+/*-----------------------------------------------------------------------------------------*/
 /* DESENHO DE PROJETEIS */
-// Função de desenho de um projetil para o jogador
+/*-----------------------------------------------------------------------------------------*/
+/* JOGADOR */
+// Função de desenho de projetil para o jogador
 void desenhar_projetil_jogador(projetil *projetil);
 void desenhar_projetil_especial_jogador_1(projetil *projetil);
 void desenhar_projetil_especial_jogador_2(projetil *projetil);
-// Função de desenho de um projetil para os inimigos
+/*-----------------------------------------------------------------------------------------*/
+/* INIMIGOS */
+// Função de desenho de projetil para os inimigos
 void desenhar_projetil_inimigo_fase_1(projetil *projetil);
 void desenhar_projetil_inimigo_fase_2(projetil *projetil);
-// Função de desenho de um projetil para os chefes
+/*-----------------------------------------------------------------------------------------*/
+// Função de desenho de projetil para os chefes
+/* CHEFE FASE 01 */
 void desenhar_projetil_chefe_0(projetil *projetil);
 void desenhar_projetil2_chefe_0(projetil *projetil);
+/*-----------------------------------------------------------------------------------------*/
+/* CHEFE FASE 02 */
 void desenhar_projetil_chefe_1(projetil *projetil);
 void desenhar_projetil2_chefe_1(projetil *projetil);
 
+/*-----------------------------------------------------------------------------------------*/
 /* ATAQUE ESPECIAL */
 
 #endif // Guardas de inclusão
