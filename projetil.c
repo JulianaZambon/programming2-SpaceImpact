f59051659e08442e7d9cc301373f5e81f2ffb4c7
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-12-01 23:30:45 -0300

    Update projetil.c

--- projetil.c
+++ projetil.c
@@ -63,6 +63,7 @@ void mover_projetil(projetil **elements)
         }
     }
 }
+
 // Função de destruição de um projetil
 void destruir_projetil(projetil *element)
 {
@@ -77,135 +78,127 @@ void destruir_projetil(projetil *element)
 
 /* JOGADOR*/
 
+/* FUNÇÕES AUXILIARES PARA DESENHO DE PROJETIL */
+
+ALLEGRO_COLOR cor(int r, int g, int b)
+{
+    return al_map_rgb(r, g, b);
+}
+
+void desenhar_retorno_pulsante(float variacao, float x, float y, ALLEGRO_COLOR cor_interna, ALLEGRO_COLOR cor_1, ALLEGRO_COLOR cor_2)
+{
+    // Função para desenhar círculos com efeito de pulsação
+    al_draw_filled_circle(x, y, 10 + variacao, cor_2);
+    al_draw_filled_circle(x, y, 8 + variacao / 2, cor_1);
+    al_draw_filled_circle(x, y, 5, cor_interna);
+}
+
+/* PROJETIL JOGADOR */
+
 void desenhar_projetil_jogador(projetil *projetil)
 {
-    ALLEGRO_COLOR cor_vermelho_escuro = al_map_rgb(139, 0, 0); // Cor interna do projetil
-    ALLEGRO_COLOR cor_vermelho_claro = al_map_rgb(255, 0, 0);  // Cor da borda do projetil
+    ALLEGRO_COLOR cor_vermelho_escuro = cor(139, 0, 0);
+    ALLEGRO_COLOR cor_vermelho_claro = cor(255, 0, 0);
 
-    // Sensação de laser:
     al_draw_filled_rectangle(projetil->x - 10, projetil->y - 3, projetil->x + 10, projetil->y + 3, cor_vermelho_escuro);
     al_draw_rectangle(projetil->x - 10, projetil->y - 3, projetil->x + 10, projetil->y + 3, cor_vermelho_claro, 1);
 }
+
 void desenhar_projetil_especial_jogador_1(projetil *projetil)
 {
-    // Cores para o projétil especial
-    ALLEGRO_COLOR cor_azul = al_map_rgb(0, 0, 255);           // Azul intenso
-    ALLEGRO_COLOR cor_branco = al_map_rgb(255, 255, 255);     // Branco brilhante
-    ALLEGRO_COLOR cor_azul_claro = al_map_rgb(173, 216, 230); // Azul claro para brilho
+    ALLEGRO_COLOR cor_azul = cor(0, 0, 255);
+    ALLEGRO_COLOR cor_branco = cor(255, 255, 255);
+    ALLEGRO_COLOR cor_azul_claro = cor(173, 216, 230);
 
-    // Desenha retângulos concêntricos com efeito de camadas
-    al_draw_filled_rectangle(projetil->x - 15, projetil->y - 4, projetil->x + 15, projetil->y + 4, cor_azul);       // Camada central azul
-    al_draw_filled_rectangle(projetil->x - 12, projetil->y - 3, projetil->x + 12, projetil->y + 3, cor_azul_claro); // Camada de azul claro
-    al_draw_filled_rectangle(projetil->x - 9, projetil->y - 2, projetil->x + 9, projetil->y + 2, cor_branco);       // Camada brilhante no centro
+    al_draw_filled_rectangle(projetil->x - 15, projetil->y - 4, projetil->x + 15, projetil->y + 4, cor_azul);
+    al_draw_filled_rectangle(projetil->x - 12, projetil->y - 3, projetil->x + 12, projetil->y + 3, cor_azul_claro);
+    al_draw_filled_rectangle(projetil->x - 9, projetil->y - 2, projetil->x + 9, projetil->y + 2, cor_branco);
 }
+
 void desenhar_projetil_especial_jogador_2(projetil *projetil)
 {
-    // Cores para o projétil especial de "raio"
-    ALLEGRO_COLOR cor_amarela = al_map_rgb(255, 255, 0);       // Amarelo intenso
-    ALLEGRO_COLOR cor_laranja = al_map_rgb(255, 140, 0);       // Laranja vibrante
-    ALLEGRO_COLOR cor_branco = al_map_rgb(255, 255, 255);      // Branco para um brilho forte
-    ALLEGRO_COLOR cor_vermelho_escuro = al_map_rgb(139, 0, 0); // Vermelho escuro para as bordas
+    ALLEGRO_COLOR cor_amarela = cor(255, 255, 0);
+    ALLEGRO_COLOR cor_laranja = cor(255, 140, 0);
+    ALLEGRO_COLOR cor_branco = cor(255, 255, 255);
+    ALLEGRO_COLOR cor_vermelho_escuro = cor(139, 0, 0);
 
-    // Desenha um efeito de raio com múltiplas camadas
-    al_draw_filled_rectangle(projetil->x - 15, projetil->y - 4, projetil->x + 15, projetil->y + 4, cor_vermelho_escuro); // Camada externa escura
-    al_draw_filled_rectangle(projetil->x - 12, projetil->y - 3, projetil->x + 12, projetil->y + 3, cor_laranja);         // Camada intermediária laranja
-    al_draw_filled_rectangle(projetil->x - 10, projetil->y - 2, projetil->x + 10, projetil->y + 2, cor_amarela);         // Camada central amarela
-    al_draw_filled_rectangle(projetil->x - 7, projetil->y - 1, projetil->x + 7, projetil->y + 1, cor_branco);            // Camada de brilho branco
+    al_draw_filled_rectangle(projetil->x - 15, projetil->y - 4, projetil->x + 15, projetil->y + 4, cor_vermelho_escuro);
+    al_draw_filled_rectangle(projetil->x - 12, projetil->y - 3, projetil->x + 12, projetil->y + 3, cor_laranja);
+    al_draw_filled_rectangle(projetil->x - 10, projetil->y - 2, projetil->x + 10, projetil->y + 2, cor_amarela);
+    al_draw_filled_rectangle(projetil->x - 7, projetil->y - 1, projetil->x + 7, projetil->y + 1, cor_branco);
 }
 
-/* INIMIGOS */
-// Função de desenho de projetil para inimigos
+/* PROJETIL INIMIGOS */
+
 void desenhar_projetil_inimigo_fase_1(projetil *projetil)
 {
-    ALLEGRO_COLOR cor_verde_escuro = al_map_rgb(0, 100, 0); // Cor interna do projetil
-    ALLEGRO_COLOR cor_verde_claro = al_map_rgb(0, 255, 0);  // Cor da borda do projetil
+    ALLEGRO_COLOR cor_verde_escuro = cor(0, 100, 0);
+    ALLEGRO_COLOR cor_verde_claro = cor(0, 255, 0);
 
-    // Sensação de laser:
     al_draw_filled_rectangle(projetil->x - 10, projetil->y - 3, projetil->x + 10, projetil->y + 3, cor_verde_escuro);
     al_draw_rectangle(projetil->x - 10, projetil->y - 3, projetil->x + 10, projetil->y + 3, cor_verde_claro, 1);
 }
+
 void desenhar_projetil_inimigo_fase_2(projetil *projetil)
 {
-    ALLEGRO_COLOR cor_rosa_escuro = al_map_rgb(255, 20, 147); // Cor interna do projetil
-    ALLEGRO_COLOR cor_rosa_claro = al_map_rgb(255, 105, 180); // Cor da borda do projetil
+    ALLEGRO_COLOR cor_rosa_escuro = cor(255, 20, 147);
+    ALLEGRO_COLOR cor_rosa_claro = cor(255, 105, 180);
 
-    // Sensação de laser:
     al_draw_filled_rectangle(projetil->x - 10, projetil->y - 3, projetil->x + 10, projetil->y + 3, cor_rosa_escuro);
     al_draw_rectangle(projetil->x - 10, projetil->y - 3, projetil->x + 10, projetil->y + 3, cor_rosa_claro, 1);
 }
 
 /* CHEFE FASE 01 */
-// Chefe com chamas, portanto dar uma sensação de bolas de fogo aos projeteis
-// Primeiro ataque unico do chefe 0
+
 void desenhar_projetil_chefe_0(projetil *projetil)
 {
-    // Variação de tamanho para pulsação da chama
     float variacao = sin(al_get_time() * 10) * 2;
 
-    ALLEGRO_COLOR cor_amarela = al_map_rgb(255, 255, 0); // Amarelo interno
-    ALLEGRO_COLOR cor_laranja = al_map_rgb(255, 140, 0); // Laranja intermediário
-    ALLEGRO_COLOR cor_vermelha = al_map_rgb(255, 69, 0); // Vermelho externo
+    ALLEGRO_COLOR cor_amarela = cor(255, 255, 0);
+    ALLEGRO_COLOR cor_laranja = cor(255, 140, 0);
+    ALLEGRO_COLOR cor_vermelha = cor(255, 69, 0);
 
-    // Desenha círculos concêntricos (com mesmo centro) com variação no tamanho
-    al_draw_filled_circle(projetil->x, projetil->y, 10 + variacao, cor_vermelha);   // Vermelho externo
-    al_draw_filled_circle(projetil->x, projetil->y, 8 + variacao / 2, cor_laranja); // Laranja intermediário
-    al_draw_filled_circle(projetil->x, projetil->y, 5, cor_amarela);                // Amarelo interno
+    desenhar_retorno_pulsante(variacao, projetil->x, projetil->y, cor_amarela, cor_laranja, cor_vermelha);
 }
-// Segundo ataque único do chefe 0
+
 void desenhar_projetil2_chefe_0(projetil *projetil)
 {
-    // Variação de tamanho para dar a sensação de pulsação ou aumento de energia
     float variacao = sin(al_get_time() * 10) * 3;
 
-    // Cores com base em chamas, usando vermelho, laranja, amarelo e uma cor de brilho
-    ALLEGRO_COLOR cor_rosa_escuro = al_map_rgb(255, 20, 147); // Rosa escuro (brilho intenso)
-    ALLEGRO_COLOR cor_vermelha = al_map_rgb(255, 69, 0);      // Vermelho (externo)
-    ALLEGRO_COLOR cor_laranja = al_map_rgb(255, 140, 0);      // Laranja (intermediário)
-    ALLEGRO_COLOR cor_amarela = al_map_rgb(255, 255, 0);      // Amarelo (interno)
+    ALLEGRO_COLOR cor_rosa_escuro = cor(255, 20, 147);
+    ALLEGRO_COLOR cor_vermelha = cor(255, 69, 0);
+    ALLEGRO_COLOR cor_laranja = cor(255, 140, 0);
+    ALLEGRO_COLOR cor_amarela = cor(255, 255, 0);
 
-    // Desenha uma aura de brilho (rosa) para o efeito do projétil
-    al_draw_filled_circle(projetil->x, projetil->y, 15 + variacao, cor_rosa_escuro); // Brilho rosa escuro ao redor
-
-    // Desenha as camadas de cores das chamas com variação de tamanho
-    al_draw_filled_circle(projetil->x, projetil->y, 12 + variacao, cor_vermelha);     // Vermelho externo
-    al_draw_filled_circle(projetil->x, projetil->y, 9 + variacao / 1.5, cor_laranja); // Laranja intermediário
-    al_draw_filled_circle(projetil->x, projetil->y, 6, cor_amarela);                  // Amarelo interno
+    al_draw_filled_circle(projetil->x, projetil->y, 15 + variacao, cor_rosa_escuro);
+    al_draw_filled_circle(projetil->x, projetil->y, 12 + variacao, cor_vermelha);
+    al_draw_filled_circle(projetil->x, projetil->y, 9 + variacao / 1.5, cor_laranja);
+    al_draw_filled_circle(projetil->x, projetil->y, 6, cor_amarela);
 }
 
 /* CHEFE FASE 02 */
-// Primeiro ataque unico do chefe 1
+
 void desenhar_projetil_chefe_1(projetil *projetil)
 {
-    // Variação de tamanho para pulsação da chama
     float variacao = sin(al_get_time() * 10) * 2;
 
-    ALLEGRO_COLOR cor_azul_claro = al_map_rgb(173, 216, 230); // Azul claro
-    ALLEGRO_COLOR cor_azul = al_map_rgb(135, 206, 235);       // Azul céu
-    ALLEGRO_COLOR cor_roxo = al_map_rgb(128, 0, 128);         // Roxo
+    ALLEGRO_COLOR cor_azul_claro = cor(173, 216, 230);
+    ALLEGRO_COLOR cor_azul = cor(135, 206, 235);
+    ALLEGRO_COLOR cor_roxo = cor(128, 0, 128);
 
-    // Desenha círculos concêntricos (com mesmo centro) com variação no tamanho
-    al_draw_filled_circle(projetil->x, projetil->y, 10 + variacao, cor_roxo);
-    al_draw_filled_circle(projetil->x, projetil->y, 8 + variacao / 2, cor_azul);
-    al_draw_filled_circle(projetil->x, projetil->y, 5, cor_azul_claro);
+    desenhar_retorno_pulsante(variacao, projetil->x, projetil->y, cor_azul_claro, cor_azul, cor_roxo);
 }
-// Segundo ataque unico do chefe 1
+
 void desenhar_projetil2_chefe_1(projetil *projetil)
 {
-    // Variação de tamanho para dar uma sensação de pulsação ou aumento de energia
-    float variacao = sin(al_get_time() * 10) * 2;
+    float variacao = sin(al_get_time() * 10) * 3;
 
-    // Cores com base em azul e roxo, com efeitos de brilho e energia
-    ALLEGRO_COLOR cor_roxo_escuro = al_map_rgb(75, 0, 130);   // Roxo escuro (energético)
-    ALLEGRO_COLOR cor_roxo = al_map_rgb(128, 0, 128);         // Roxo (intermediário)
-    ALLEGRO_COLOR cor_azul = al_map_rgb(100, 149, 237);       // Azul (centrado)
-    ALLEGRO_COLOR cor_azul_claro = al_map_rgb(173, 216, 230); // Azul claro (brilho externo)
+    ALLEGRO_COLOR cor_azul = cor(0, 0, 255);
+    ALLEGRO_COLOR cor_azul_claro = cor(173, 216, 230);
+    ALLEGRO_COLOR cor_branco = cor(255, 255, 255);
 
-    // Desenha a aura externa do projétil, com cor roxa mais escura
-    al_draw_filled_circle(projetil->x, projetil->y, 15 + variacao, cor_roxo_escuro); // Roxo escuro externo
-    // Desenha a camada intermediária do projétil, com cor roxa
-    al_draw_filled_circle(projetil->x, projetil->y, 12 + variacao / 1.5, cor_roxo); // Roxo intermediário
-    // Desenha a camada de azul, representando o núcleo do projétil
-    al_draw_filled_circle(projetil->x, projetil->y, 9, cor_azul); // Azul central
-    // Camada final de azul claro, criando um brilho suave ao redor
-    al_draw_filled_circle(projetil->x, projetil->y, 7, cor_azul_claro); // Azul claro externo
+    al_draw_filled_circle(projetil->x, projetil->y, 15 + variacao, cor_azul);
+    al_draw_filled_circle(projetil->x, projetil->y, 12 + variacao, cor_azul_claro);
+    al_draw_filled_circle(projetil->x, projetil->y, 9 + variacao / 1.5, cor_branco);
+    al_draw_filled_circle(projetil->x, projetil->y, 6, cor_azul_claro);
 }
