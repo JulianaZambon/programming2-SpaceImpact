cf9f7e33da26bc2f2326582b592a63bb34750241
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-12-07 01:03:47 -0300

    Update background.h

--- background.h
+++ background.h
@@ -3,7 +3,7 @@
 
 #include <allegro5/allegro.h> // Inclusão necessária para usar ALLEGRO_BITMAP
 
-/*-----------------------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------*/
 /* PROTÓTIPO DE FUNÇÕES */
 void atualiza_e_desenha_background(ALLEGRO_BITMAP *background, float *background_x, float velocidade);
 
